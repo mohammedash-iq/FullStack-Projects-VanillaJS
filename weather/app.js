9e1f3663ef0c8455d8373719ed221a3cc0c22bb5
m0hammedashiq <mohammedashiq206@gmail.com>
2025-01-12 11:43:15 +0530

    made some changes to weather app

--- weather/app.js
+++ weather/app.js
@@ -1,6 +1,8 @@
 const searchBtn =document.querySelector("#searchBtn");
 const searchInput = document.querySelector('#searchInput')
+const url ="http://api.weatherstack.com/current? access_key = YOUR_ACCESS_KEY& query = New York";
+let promise =fetch(url)
 searchBtn.addEventListener('click',()=>{
     console.log(searchInput.value);
-    
+
 })
