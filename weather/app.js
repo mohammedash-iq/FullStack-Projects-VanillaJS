7648e7b651d7fd080498842720d0a8c47d653237
Mohammed Ash <mohammedashiq206@gmail.com>
2025-05-03 06:17:42 +0000

    restructured the repo

--- weather/app.js
+++
@@ -1,8 +0,0 @@
-const searchBtn =document.querySelector("#searchBtn");
-const searchInput = document.querySelector('#searchInput')
-const url ="http://api.weatherstack.com/current? access_key = YOUR_ACCESS_KEY& query = New York";
-let promise =fetch(url)
-searchBtn.addEventListener('click',()=>{
-    console.log(searchInput.value);
-
-})
